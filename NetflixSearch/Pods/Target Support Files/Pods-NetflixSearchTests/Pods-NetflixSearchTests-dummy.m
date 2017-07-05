61277c866d82b36e57380de9b1372d7ea960f1e9
Williej926 <williej926@gmail.com>
2017-07-05 15:13:02 -0700

    test

--- NetflixSearch/Pods/Target Support Files/Pods-NetflixSearchTests/Pods-NetflixSearchTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_NetflixSearchTests : NSObject
-@end
-@implementation PodsDummy_Pods_NetflixSearchTests
-@end
--- NetflixSearch/Pods/Target Support Files/Pods-NetflixSearchUITests/Pods-NetflixSearchUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_NetflixSearchUITests : NSObject
-@end
-@implementation PodsDummy_Pods_NetflixSearchUITests
-@end
